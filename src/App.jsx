baecfd6683f00ce5a63c43b2536afd61c83a0b6a
Graham <graham@pricesquawk.com>
2025-08-14 14:54:08 +1000

    Add vercel speed insights and analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import EconomicCalendar from './components/features/EconomicCalendar';
 import { ThemeProvider } from './hooks/useTheme.jsx';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <EconomicCalendar />
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <EconomicCalendar />
+      </ThemeProvider>
+      <SpeedInsights />
+      <Analytics />
+    </>
   );
 }
