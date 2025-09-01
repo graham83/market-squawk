d596517979ef86b61d0ee3161321701c760ef753
Graham <graham@pricesquawk.com>
2025-09-01 11:29:45 +1000

    Clean up old vite code as now using next js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../src/index.css';
+import '../styles/globals.css';
 import { ThemeProvider } from '../hooks/useTheme.jsx';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
