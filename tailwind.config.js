3088cb95932a705d36586c84d0d88ac75511acec
Graham <graham@pricesquawk.com>
2025-09-01 11:29:45 +1000

    Clean up old vite code as now using next js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 export default withMT({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}"
+  ],
   darkMode: 'class',
   theme: {
     extend: {},
